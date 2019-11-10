0ef70017bf650c2d35da10519e0960306eadd613
1612041_phbinh <1612041_phbinh>
2019-11-11 04:18:54 +0700

    fix coin's bug

---
+++ Assets/Script/GameObjects/Coin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public GameMaster gameMaster;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameMaster = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            gameMaster.score += 1;
+        }
+    }
+}
