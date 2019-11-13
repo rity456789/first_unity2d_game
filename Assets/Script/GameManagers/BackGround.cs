5965bfa1f5ac98f193b433f67d119bebc30bcc7c
1612041_phbinh <1612041_phbinh>
2019-11-13 11:08:38 +0700

    add background

---
+++ Assets/Script/GameManagers/BackGround.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    public Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        Material material = meshRenderer.material;
+        Vector2 offset = material.mainTextureOffset;
+        offset.x = player.transform.position.x / 2;
+        material.mainTextureOffset = offset * Time.fixedDeltaTime;
+    }
+}
---
+++ Assets/Script/GameManagers/GameMaster.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour
+{
+    public int score = 0;
+    public int highScore = 0;
+
+    public Text scoreText;
+    public Text highScoreText;
+    public Text newSceneText;
+
+    public Player player;
+
+    public int curBuildIndexScene = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponentInParent<Player>();
+
+        highScoreText.text = ("HighScore: " + PlayerPrefs.GetInt("highScore"));
+        highScore = PlayerPrefs.GetInt("highScore", 0);
+ 
+        if (PlayerPrefs.HasKey("score"))
+        {
+            Scene activeScreen = SceneManager.GetActiveScene();
+            if (activeScreen.buildIndex == curBuildIndexScene)
+            {
+                PlayerPrefs.DeleteKey("score");
+                score = 0;
+            }
+            else
+                score = PlayerPrefs.GetInt("score");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = ("Score: " + score);
+        if (player.curHP <= 0)
+        {
+            if (PlayerPrefs.GetInt("highScore") < score) PlayerPrefs.SetInt("highScore", score);
+            PlayerPrefs.DeleteKey("score");
+        }
+    }
+}
