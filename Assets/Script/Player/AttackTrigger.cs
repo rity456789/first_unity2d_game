2cec5d1722374e3fca8637492a42373f7620278b
1612041_phbinh <1612041_phbinh>
2019-11-14 17:07:22 +0700

    delay attack trigger due to animation

--- Assets/Script/Player/AttackTrigger.cs
+++ Assets/Script/Player/AttackTrigger.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class AttackTrigger : MonoBehaviour
 {
-    public int dmg = 20;
- 
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.isTrigger != true && col.CompareTag("Enemy"))
-        {
-            col.SendMessageUpwards("Damage", dmg);
-        }
-    }
+    public int dmg = 20;  
     
     // Start is called before the first frame update
     void Start()
@@ -24,5 +16,21 @@ public class AttackTrigger : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        //delay 0.1s due to the animation
+        if (col.isTrigger != true && col.CompareTag("Enemy"))
+        {
+            StartCoroutine(delayForAnimation(col, 0.1f));            
+        }
+    }
+
+    IEnumerator delayForAnimation (Collider2D col, float time)
+    {
+        yield return new WaitForSeconds(time);
+        col.SendMessageUpwards("Damage", dmg);
+        yield return 0;
     }
 }
